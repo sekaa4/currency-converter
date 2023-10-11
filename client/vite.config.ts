afd7c3a835cab106e4be6e8b1becf878e0e04aa9
Sergey P. <s.pansevi4@gmail.com>
2023-10-12 00:03:16 +0300

    feat: add configuration to work with docker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,16 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+const PORT = Number(process.env.PORT) ?? 5000;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: PORT,
+  },
 });
