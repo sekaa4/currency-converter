7ec9b3cb6f08b4c45137f81e62acfb81ca5fc8f0
Sergey P. <s.pansevi4@gmail.com>
2023-10-13 00:50:15 +0300

    feat: add package to server side for work with swagger and .env files

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,36 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configDotEnv = app.get(ConfigService);
+  const port = configDotEnv.get<number>('PORT');
+
+  const config = new DocumentBuilder()
+    .setTitle('Currency Service')
+    .setDescription('Currency Converter API with CRUD functionality')
+    .setVersion('1.0')
+    .addServer(`http://localhost:${port}`)
+    .addServer(`https://hls-lw2o.onrender.com`)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      stopAtFirstError: true,
+    }),
+  );
+
+  app.enableCors();
+  SwaggerModule.setup('doc', app, document);
+  await app.listen(port || 3000);
 }
 
 bootstrap();
