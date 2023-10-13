f6cf9a813e7577c9f15d14fc0cb6db2e205b7571
Sergey P. <s.pansevi4@gmail.com>
2023-10-13 19:01:53 +0300

    feat: add database resource to the application

---
+++ server/src/database/database.module.ts
@@ -0,0 +1,12 @@
+import { Module, Global } from '@nestjs/common';
+
+import { DatabaseController } from './database.controller';
+import { DatabaseService } from './database.service';
+
+@Global()
+@Module({
+  controllers: [DatabaseController],
+  providers: [DatabaseService],
+  exports: [DatabaseService],
+})
+export class DatabaseModule {}
