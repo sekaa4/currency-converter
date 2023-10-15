23626e477bb1a60f75fea85eb81a351c1c184b5b
Sergey P. <s.pansevi4@gmail.com>
2023-10-15 13:36:09 +0300

    feat: add error boundary component to the application

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,12 +3,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './providers';
 
 import { StoreProvider } from './providers/store-provider';
+
 import './styles/App.css';
+import ErrorBoundary from '@/shared/ui/error-boundary/error-boundary';
+import { FallBackError } from '@/shared/ui/fall-back-error/fall-back-error';
 
 export const App = () => (
   <BrowserRouter>
-    <StoreProvider>
-      <AppRouter />
-    </StoreProvider>
+    <ErrorBoundary fallBackUIComponent={<FallBackError />}>
+      <StoreProvider>
+        <AppRouter />
+      </StoreProvider>
+    </ErrorBoundary>
   </BrowserRouter>
 );
